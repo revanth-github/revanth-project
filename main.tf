3f0c9459b0f81e37b7b41818d7fcc6d327f30526
revanth pullagaram <revanth.pullagaram@aig.com>
2021-04-19 13:18:37 -0500

    added

--- main.tf
+++ main.tf
@@ -6,26 +6,26 @@ variable "vpc_id" {
   default = "vpc-3e34b743"
 }
 variable "infrastructure_version" {
-  default = "2"
+  default = "3"
 }
 terraform {
   backend "s3" {
     encrypt = true
     bucket  = "terraform-bluegreen-testhelloworld"
     region  = "us-east-1"
-    key     = "v2"
+    key     = "v3"
   }
 }
 locals {
-  subnet_count       = 1
-  availability_zones = ["us-east-1a"]
+  subnet_count       = 2
+  availability_zones = ["us-east-1a", "us-east-1b"]
 }
 
-resource "aws_subnet" "terraform-blue-greenv2" {
+resource "aws_subnet" "terraform-blue-green" {
   count                   = "${local.subnet_count}"
   vpc_id                  = "${var.vpc_id}"
   availability_zone       = "${element(local.availability_zones, count.index)}"
-  cidr_block              = "172.31.${local.subnet_count * (var.infrastructure_version - 0) + count.index + 2}.0/24"
+  cidr_block              = "172.31.${local.subnet_count * (var.infrastructure_version - 1) + count.index + 1}.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -33,34 +33,47 @@ resource "aws_subnet" "terraform-blue-greenv2" {
   }
 }
 
-locals {
-  subnets = "${aws_subnet.terraform-blue-greenv2.*.id}"
+resource "aws_alb" "terraform" {
+  name                                = "terraform-bluegreen"
+  internal                            = false
+  security_groups                     = ["${aws_security_group.instance.id}"]
+  subnets                             = "${aws_subnet.terraform-blue-green.*.id}"
+  enable_deletion_protection          = false
+}
+resource "aws_lb_target_group" "terraform" {
+    depends_on                          = ["aws_alb.terraform"]
+    name                                = "terraform-bluegreen"
+    port                                = 80
+    protocol                            = "HTTP"
+    vpc_id                              = "${var.vpc_id}"
+    deregistration_delay                = 10
+    health_check {
+      protocol                          = "HTTP"
+      path                              = "/"
+      port                              = "80"
+      healthy_threshold                 = 2
+      unhealthy_threshold               = 10
+      timeout                           = 4
+      interval                          = 5
+      matcher                           = "200"
+    }
   }
 
-resource "aws_instance" "examplev2" {
-    count  = 1
-    ami = "ami-20ff515a"
-    instance_type = "t2.small"
-    subnet_id              = "${element(local.subnets, count.index)}"
-    vpc_security_group_ids = ["${aws_security_group.instancev2.id}"]
-    key_name               = "terraform-bluegreen"
-# add
-    user_data = <<-EOF
-                #! /bin/bash
-                sudo yum update
-                sudo yum install -y httpd
-                sudo chkconfig httpd on
-                sudo service httpd start
-                echo "<h1>hello world</h1>" | sudo tee /var/www/html/index.html
-                EOF
+  resource "aws_alb_listener" "terraform" {
+    depends_on                          = ["aws_alb.terraform", "aws_lb_target_group.terraform"]
+    load_balancer_arn                   = "${aws_alb.terraform.arn}"
+    port                                = "80"
+    protocol                            = "HTTP"
 
-    tags = {
-        Name = "terraform-blue"
+
+    default_action {
+      target_group_arn                  = "${aws_lb_target_group.terraform.arn}"
+      type                              = "forward"
     }
-}
+  }
 
-resource "aws_security_group" "instancev2" {
-    name = "terraform-example-instancev2"
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
     ingress {
         from_port = 80
         to_port = 80
@@ -75,10 +88,3 @@ resource "aws_security_group" "instancev2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-
-  resource "aws_lb_target_group_attachment" "terraform-1" {
-    target_group_arn                    = "arn:aws:elasticloadbalancing:us-east-1:376048985616:targetgroup/terraform-bluegreen/0b7aabebcfae1ac9"
-    target_id                           = "${aws_instance.examplev2.0.id}"
-    port                                = 80
-  }
